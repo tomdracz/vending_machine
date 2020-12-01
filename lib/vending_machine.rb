532693daeec10da8c4a758499ff030e746102700
Tom Dracz <tjdracz@gmail.com>
2020-12-01 19:32:21 +0000

    Implement grabbing user selection

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,14 +1,25 @@
 class VendingMachine
-  attr_reader :inventory, :change
+  attr_reader :inventory, :change, :customer_selection
 
   def initialize(inventory, change)
     @inventory = inventory
     @change = change
+    @customer_selection = customer_selection
   end
 
   def display_inventory
     inventory.each_with_index do |product, index|
       puts format_product_for_display(product, index)
+    end
+  end
+
+  def get_customer_selection
+    selection = STDIN.gets.chomp
+    product_index = selection.to_i - 1
+    if product_index >= 0
+      return @customer_selection = product_index
+    else
+      puts "Invalid code selected. Please try again"
     end
   end
 
