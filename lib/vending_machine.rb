ece812ab1dc7e60f31b0162c9c5823d1f83ba4dc
Tom Dracz <tjdracz@gmail.com>
2020-12-01 19:16:52 +0000

    Add method to display vending machine's products to customer

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -5,4 +5,16 @@ class VendingMachine
     @inventory = inventory
     @change = change
   end
+
+  def display_inventory
+    inventory.each_with_index do |product, index|
+      puts format_product_for_display(product, index)
+    end
+  end
+
+  private
+
+  def format_product_for_display(product, index)
+    "Code #{index + 1}: #{product.name} - Price: #{product.price}"
+  end
 end
