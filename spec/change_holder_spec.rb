8bcf7ca01138872393e23b485114d8353443012f
Tom Dracz <tjdracz@gmail.com>
2020-12-01 18:36:35 +0000

    Add specs for returning coins from coin holder

--- spec/change_holder_spec.rb
+++ spec/change_holder_spec.rb
@@ -37,4 +37,27 @@ RSpec.describe ChangeHolder do
       expect { subject.insert_coin(140) }.to raise_error(/Invalid coin provided/)
     end
   end
+
+  describe '#return_coin' do
+    it 'returns array of returned coin values' do
+      expect(subject.return_coin(100, 3)).to eq([100, 100, 100])
+    end
+    it 'decrements the desired coin value by a given amount' do
+      expect { subject.return_coin(100, 3) }.to change { subject.coins[100] }.by(-3)
+      expect(subject.coins[100]).to eq(17)
+    end
+
+    it 'decrements the desired coin value by 1 if amount is omitted' do
+      expect { subject.return_coin(100) }.to change { subject.coins[100] }.by(-1)
+      expect(subject.coins[100]).to eq(19)
+    end
+
+    it 'raises an error if an invalid coin value is provided' do
+      expect { subject.insert_coin(140) }.to raise_error(/Invalid coin provided/)
+    end
+
+    it 'raises an error if the amount of coins requested cannot be returned' do
+      expect { subject.return_coin(50, 100) }.to raise_error(/Cannot return the requested amount of coins/)
+    end
+  end
 end
