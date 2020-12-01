9de8b380995f1390da03bd62245b31f0fa04844f
Tom Dracz <tjdracz@gmail.com>
2020-12-01 18:19:17 +0000

    Add initial initialize test for CoinHolder class

---
+++ spec/change_holder_spec.rb
@@ -0,0 +1,22 @@
+RSpec.describe ChangeHolder do
+  let(:coins) do
+    {
+      200 => 20,
+      100 => 20,
+      50 => 20,
+      20 => 20,
+      10 => 20,
+      5 => 20,
+      2 => 20,
+      1 => 20
+    }
+  end
+  subject { described_class.new(coins) }
+
+  describe '#initialize' do
+    it 'has coins property which is a hash of key/value pairs of coins pence value and quantity' do
+      expect(subject.coins).to be_a(Hash)
+      expect(subject.coins).to eq(coins)
+    end
+  end
+end
