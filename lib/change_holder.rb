3bd16b76661905697ec60ad6a44d63e371980d0a
Tom Dracz <tjdracz@gmail.com>
2020-12-01 18:31:34 +0000

    Implement #insert_coin method

--- lib/change_holder.rb
+++ lib/change_holder.rb
@@ -1,7 +1,16 @@
 class ChangeHolder
+  VALID_COIN_VALUES = [200, 100, 50, 10, 20, 5, 2, 1].freeze
+
   attr_accessor :coins
 
   def initialize(coins)
     @coins = coins
+  end
+
+  def insert_coin(value, amount = 1)
+    unless VALID_COIN_VALUES.include?(value)
+      raise "Invalid coin provided"
+    end
+    @coins[value] += amount
   end
 end
