f5ecb6372a904c7061576afeb8e52e446eefed65
Tom Dracz <tjdracz@gmail.com>
2020-12-01 19:11:24 +0000

    Add spec for displaying products from vending machine's inventory

--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -1,4 +1,5 @@
-require_relative "../lib/vending_machine"
+require_relative '../lib/vending_machine'
+require_relative '../lib/product'
 
 RSpec.describe VendingMachine do
   let(:inventory) { double('inventory') }
@@ -11,6 +12,22 @@ RSpec.describe VendingMachine do
 
     it 'has a change' do
       expect(subject.change).to eq(change)
+    end
+  end
+
+  describe '#display_inventory' do
+    let(:inventory) do
+      [
+        Product.new('Crisps', 60, 10),
+        Product.new('Coke', 70, 10),
+        Product.new('Dr Pepper', 70, 0)
+      ]
+    end
+    it 'shows the selection of current products with their prices' do
+      expectation = expect { subject.display_inventory }
+      expectation.to output(/Crisps.*60/).to_stdout
+      expectation.to output(/Coke.*70/).to_stdout
+      expectation.to output(/Dr Pepper.*70/).to_stdout
     end
   end
 end
