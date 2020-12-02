d80e6addf644142abeb2f567044c9105f4a1752c
Tom Dracz <tjdracz@gmail.com>
2020-12-02 13:37:46 +0000

    Add implementation for reloading change

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -25,7 +25,7 @@ class VendingMachine
   end
 
   def display_welcome_message
-    puts "Welcome to the Vending Machine. Please see the products available"
+    puts 'Welcome to the Vending Machine. Please see the products available'
   end
 
   def display_inventory
@@ -35,10 +35,11 @@ class VendingMachine
   end
 
   def get_customer_selection
-    puts "Please enter the code of the item you wish to purchase"
+    puts 'Please enter the code of the item you wish to purchase'
 
     loop do
       break if @customer_selection
+
       selection = STDIN.gets.chomp
       product_index = selection.to_i - 1
       if product_index >= 0 && product_index < inventory.size && inventory[product_index].available?
@@ -51,11 +52,12 @@ class VendingMachine
   end
 
   def collect_coins
-    puts "Please insert your coins entering pence value of a coin (i.e. 100 for £1)"
+    puts 'Please insert your coins entering pence value of a coin (i.e. 100 for £1)'
     price = selected_product.price
     collected_coins = []
     loop do
       break if collected_coins.sum >= price
+
       puts "Product price is: #{price}. Inserted #{collected_coins.sum} so far"
 
       inserted_coin = STDIN.gets.chomp.to_i
@@ -77,7 +79,7 @@ class VendingMachine
   def return_change
     inserted_sum = inserted_coins.sum
     product_price = selected_product.price
-  
+
     return if inserted_sum == product_price
 
     returned_change = change.calculate_change(product_price, inserted_sum)
@@ -85,14 +87,15 @@ class VendingMachine
   end
 
   def reload_products
-    puts "Here are the current products"
+    puts 'Here are the current products'
     display_inventory
-    puts "Please enter the code of the item you want to reload"
+    puts 'Please enter the code of the item you want to reload'
     reload_selection = nil
     reload_quantity = nil
 
     loop do
       break if reload_selection
+
       selection_input = STDIN.gets.chomp
       product_index = selection_input.to_i - 1
       if product_index >= 0 && product_index < inventory.size
@@ -102,25 +105,60 @@ class VendingMachine
       end
     end
 
-    puts "Enter the quantity you are reloading the item by"
+    puts 'Enter the quantity you are reloading the item by'
 
     loop do
       break if reload_quantity
+
       quantity_input = STDIN.gets.chomp
       if quantity_input.to_i > 0
         reload_quantity = quantity_input.to_i
       else
-        puts "Invalid quantity entered. Must be a valid number above 0"
+        puts 'Invalid quantity entered. Must be a valid number above 0'
       end
     end
-    
-    if reload_selection && reload_quantity
-      inventory[reload_selection].restock(reload_quantity)
-    end
+
+    inventory[reload_selection].restock(reload_quantity) if reload_selection && reload_quantity
     reload_selection = nil
     reload_quantity = nil
 
-    puts "Restocked!"
+    puts 'Restocked!'
+  end
+
+  def reload_change
+    puts 'Please enter the pence value of coin you wish to reload (i.e. 100 for £1)'
+    reload_selection = nil
+    reload_quantity = nil
+
+    loop do
+      break if reload_selection
+
+      selection_input = STDIN.gets.chomp.to_i
+      if ChangeHolder::VALID_COIN_VALUES.include?(selection_input)
+        reload_selection = selection_input
+      else
+        puts 'Invalid coin value selected. Please try again'
+      end
+    end
+
+    puts 'Enter the quantity you are reloading the coins by'
+
+    loop do
+      break if reload_quantity
+
+      quantity_input = STDIN.gets.chomp
+      if quantity_input.to_i > 0
+        reload_quantity = quantity_input.to_i
+      else
+        puts 'Invalid quantity entered. Must be a valid number above 0'
+      end
+    end
+
+    change.insert_coin(reload_selection, reload_quantity) if reload_selection && reload_quantity
+    reload_selection = nil
+    reload_quantity = nil
+
+    puts 'Coins reloaded!'
   end
 
   private
